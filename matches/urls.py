3568cfce4b28df719fbb0b8fce788ec8b34ac41b
alextrand <stratienko1986@gmail.com>
2019-03-07 04:53:57 +0200

    6th commit

--- matches/urls.py
+++ matches/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
 app_name = 'matches'
 urlpatterns = [
-    # path('upload/', upload),
     path('all_matches/', all_matches, name='all-matches'),
     path('load_excel/', load_excel, name='load-excel'),
     path('delete/', delete, name='delete-all'),
--- matches/views.py
+++ matches/views.py
@@ -1,36 +1,9 @@
-# from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.core.files.storage import FileSystemStorage
 from django.views import View
 from .excel import excel_to_db
 import pandas as pd
 from .models import Match
-
-
-class Index(View):
-    template_name = 'matches/all_matches.html'
-    context = {}
-
-    def get(self, request):
-        if request.GET.get('delete_btn') == 'Delete':
-            Match.objects.all().delete()
-        return render(request, self.template_name, self.context)
-
-
-class Load_excel(View):
-    template_name = 'index.html'
-    context = {}
-
-    def post(self, request):
-        if request.POST.get('upload_btn') == 'Upload':
-            uploaded_file = request.FILES['document']
-            fs = FileSystemStorage()
-            if fs.exists(uploaded_file.name):
-                fs.delete(uploaded_file.name)
-            fs.save(uploaded_file.name, uploaded_file)
-            df = pd.read_excel(uploaded_file, sheet_name='all')
-            excel_to_db(df)
-        return render(request, self.template_name, self.context)
 
 
 def index(request):
@@ -82,15 +55,3 @@ def delete(request):
     if request.method == 'GET':
         Match.objects.all().delete()
     return render(request, 'matches/all_matches.html', ctx)
-
-# def upload(request):
-#     if request.method == 'POST':
-#         uploaded_file = request.FILES['document']
-#         fs = FileSystemStorage()
-#         if fs.exists(uploaded_file.name):
-#             fs.delete(uploaded_file.name)
-#         fs.save(uploaded_file.name, uploaded_file)
-#
-#         df = pd.read_excel(uploaded_file, sheet_name='all')
-#         excel_to_db(df)
-#     return render(request, 'index.html')
